d8781df5c2aae795dd34f34228d02494b80c5c3b
Shaun Hamilton <shauhami020@gmail.com>
2022-09-14 14:44:41 +0100

    minor(1.6.0): version and hot-ignore

---
+++ .freeCodeCamp/client/components/error.tsx
@@ -0,0 +1,23 @@
+export const E44o5 = ({ text }: { text: string }) => {
+  return (
+    <main className='e44o5'>
+      <header>Error 4XX - 5XX</header>
+      <p>{text}</p>
+      <h2>To Keep Learning:</h2>
+      <ul>
+        <li>First, try refresh this page</li>
+      </ul>
+      <h3>Otherwise:</h3>
+      <ol>
+        <li>Open the command palette</li>
+        <li>
+          Select the <code>freeCodeCamp: Shutdown Course</code> command
+        </li>
+        <li>Open the command palette</li>
+        <li>
+          Select the <code>freeCodeCamp: Run Course</code> command
+        </li>
+      </ol>
+    </main>
+  );
+};
