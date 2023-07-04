7ac3844f915cb30f86db9b44ff793cc2c24b2d82
Shaun Hamilton <shauhami020@gmail.com>
2023-07-04 23:41:36 +0200

    chore: use marked-highlight in marked

--- .freeCodeCamp/client/utils/index.ts
+++ .freeCodeCamp/client/utils/index.ts
@@ -1,14 +1,22 @@
 import { marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
 import Prism from 'prismjs';
 
-marked.setOptions({
-  highlight: (code, lang: keyof typeof Prism['languages']) => {
-    if (Prism.languages[lang]) {
-      return Prism.highlight(code, Prism.languages[lang], String(lang));
-    } else {
-      return code;
+marked.use(
+  markedHighlight({
+    highlight: (code, lang: keyof (typeof Prism)['languages']) => {
+      if (Prism.languages[lang]) {
+        return Prism.highlight(code, Prism.languages[lang], String(lang));
+      } else {
+        return code;
+      }
     }
-  }
+  })
+);
+
+marked.use({
+  mangle: false,
+  headerIds: false
 });
 
 export function parseMarkdown(markdown: string) {
